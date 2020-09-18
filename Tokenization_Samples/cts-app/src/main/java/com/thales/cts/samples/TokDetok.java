c19cd7e26316d9ead1870467d20cd91e9b97dd59
vinay <vinay.negi@thalesesecurity.com>
2020-09-18 16:17:37 +0000

    Added CTS java Tokenization/Detokenization samples

---
+++ Tokenization_Samples/cts-app/src/main/java/com/thales/cts/samples/TokDetok.java
@@ -0,0 +1,82 @@
+package com.thales.cts.samples;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.apache.commons.codec.binary.Base64;
+import com.jayway.jsonpath.*;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+
+public class TokDetok {
+    @SuppressWarnings("null")
+    public void DoIt(String https_url, String credRaw, String data, String tokenGroup, String tokenTemplate) {
+        String credential = Base64.encodeBase64String(credRaw.getBytes());
+        try {
+            // Tokenize request
+            URL myurl = new URL(https_url + "tokenize");
+            HttpsURLConnection con = (HttpsURLConnection) myurl.openConnection();
+            String jStr = "{\"data\":\"" + data + "\",\"tokengroup\":\"" + tokenGroup + "\",\"tokentemplate\":\"" + tokenTemplate + "\"}";
+            con.setRequestProperty("Content-length", String.valueOf(jStr.length()));
+            con.setRequestProperty("Content-Type", "application/json");
+            con.setRequestProperty("Authorization", "Basic " + credential);
+            con.setRequestMethod("POST");
+            con.setDoOutput(true);
+            con.setDoInput(true);
+            DataOutputStream output = new DataOutputStream(con.getOutputStream());
+            output.writeBytes(jStr);
+            output.close();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String line = "";
+            String strResponse = "";
+
+            while ((line = rd.readLine()) != null) {
+                strResponse = strResponse + line;
+            }
+            rd.close();
+            String token = JsonPath.read(strResponse, "$.token").toString();
+            token = token.replace("\\", "\\\\");
+            token = token.replace("\"", "\\\"");
+            con.disconnect();
+            System.out.println("Tokenize server: " + https_url);
+            System.out.println("Tokenize request: " + jStr);
+            System.out.println("Tokenize response: " + strResponse);
+
+            // Detokenize request
+            myurl = new URL(https_url + "detokenize");
+            con = (HttpsURLConnection) myurl.openConnection();
+            jStr = "{\"token\":\"" + token + "\",\"tokengroup\" :\"" + tokenGroup + "\",\"tokentemplate\":\"" + tokenTemplate + "\"}";
+            System.out.println("Token : " + jStr);
+            con.setRequestProperty("Content-length", String.valueOf(jStr.length()));
+            con.setRequestProperty("Content-Type", "application/json");
+            con.setRequestProperty("Authorization", "Basic " + credential);
+            con.setRequestMethod("POST");
+            con.setDoOutput(true);
+            con.setDoInput(true);
+            output = new DataOutputStream(con.getOutputStream());
+            output.writeBytes(jStr);
+            output.close();
+            rd = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            line = "";
+            strResponse = "";
+
+            while ((line = rd.readLine()) != null) {
+                strResponse = strResponse + line;
+            }
+            rd.close();
+            con.disconnect();
+            System.out.println("Detokenize server: " + https_url);
+            System.out.println("Detokenize request: " + jStr);
+            System.out.println("Detokenize response: " + strResponse);
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Tokenization_Samples/cts-app/src/main/java/com/thales/cts/samples/TokDetokBulk.java
@@ -0,0 +1,155 @@
+package com.thales.cts.samples;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.FilenameUtils;
+import com.jayway.jsonpath.*;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+
+// read write in file, single line per entry, line saperated inputs
+public class TokDetokBulk {
+    @SuppressWarnings("null")
+    public void DoIt(String https_url,String credRaw,String filePath, String tokenGroup, String tokenTemplate) {
+        String credential = Base64.encodeBase64String(credRaw.getBytes());
+        ArrayList<String> dataBulk = new ArrayList<String>();
+        String filePathTmp;
+        try {
+            File inputFile = new File(filePath);
+            filePathTmp = inputFile.getParent() + "/" + FilenameUtils.removeExtension(inputFile.getName());
+            Scanner fileReader = new Scanner(inputFile);
+            while (fileReader.hasNextLine()) {
+                String rawData = fileReader.nextLine();
+                dataBulk.add(rawData);
+            }
+            fileReader.close();
+          } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+            return;
+        }
+        try {
+            //Bulk Tokenize request
+            URL myurl = new URL(https_url + "tokenize");
+            String jStrArray = "[";
+            for (int i = 0; i < dataBulk.size(); i++) {
+                jStrArray = jStrArray + "{\"data\":\"" + dataBulk.get(i)
+                        + "\",\"tokengroup\":\"" + tokenGroup + "\",\"tokentemplate\":\"" + tokenTemplate + "\"}";
+                if (i < dataBulk.size() - 1) {
+                    jStrArray = jStrArray + ",";
+                }
+            }
+            jStrArray = jStrArray + "]";
+
+            HttpsURLConnection con = (HttpsURLConnection) myurl.openConnection();
+            con.setRequestProperty("Content-length", String.valueOf(jStrArray.length()));
+            con.setRequestProperty("Content-Type", "application/json");
+            con.setRequestProperty("Authorization", "Basic " + credential);
+            con.setRequestMethod("POST");
+            con.setDoOutput(true);
+            con.setDoInput(true);
+            DataOutputStream output = new DataOutputStream(con.getOutputStream());
+            output.writeBytes(jStrArray);
+            output.close();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String line = "";
+            String strResponse = "";
+
+            while ((line = rd.readLine()) != null) {
+                strResponse = strResponse + line;
+            }
+            rd.close();
+            System.out.println("Bulk Tokenize request: " + jStrArray);
+            System.out.println("Bulk Tokenize response: " + strResponse);
+
+            try {
+                FileWriter fileWriter = new FileWriter(filePathTmp + "_tokenized.txt");
+                for (int i = 0; i < dataBulk.size() ; i++) {
+                    String tokenTmp = JsonPath.read(strResponse, "$[" + i + "].token").toString();
+                    tokenTmp = tokenTmp.replace("\\", "\\\\");
+                    tokenTmp = tokenTmp.replace("\"", "\\\"");
+                    fileWriter.write(tokenTmp + "\n");
+                }
+                fileWriter.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+                return;
+            }
+
+            // Bulk Detokenize request
+            myurl = new URL(https_url + "detokenize");
+            int totalTokens = 0;
+            jStrArray = "[";
+            try {
+                File inputFile = new File(filePathTmp + "_tokenized.txt");
+                Scanner fileReader = new Scanner(inputFile);
+                while (fileReader.hasNextLine()) {
+                    String token = fileReader.nextLine();
+                    jStrArray = jStrArray + "{\"token\":\"" + token
+                                + "\",\"tokengroup\" :\"" + tokenGroup 
+                                + "\",\"tokentemplate\":\"" + tokenTemplate 
+                                + "\"}";
+                    if (fileReader.hasNextLine()) {
+                        jStrArray = jStrArray + ",";
+                    }
+                    totalTokens++;
+                }
+                fileReader.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+                return;
+            }
+            jStrArray = jStrArray + "]";
+            con = (HttpsURLConnection) myurl.openConnection();
+            con.setRequestProperty("Content-length", String.valueOf(jStrArray.length()));
+            con.setRequestProperty("Content-Type", "application/json");
+            con.setRequestProperty("Authorization", "Basic " + credential);
+            con.setRequestMethod("POST");
+            con.setDoOutput(true);
+            con.setDoInput(true);
+            output = new DataOutputStream(con.getOutputStream());
+            output.writeBytes(jStrArray);
+            output.close();
+            rd = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            line = "";
+            strResponse = "";
+            while ((line = rd.readLine()) != null) {
+                strResponse = strResponse + line;
+            }
+            rd.close();
+            con.disconnect();
+            System.out.println("Bulk Detokenize request: " + jStrArray);
+            System.out.println("Bulk Detokenize response: " + strResponse);
+
+            try {
+                FileWriter fileWriter = new FileWriter(filePathTmp + "_detokenized.txt");
+                for (int i = 0; i < totalTokens ; i++) {
+                    String dataTmp = JsonPath.read(strResponse, "$[" + i + "].data").toString();
+                    fileWriter.write(dataTmp + "\n");
+                }
+                fileWriter.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+                return;
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
