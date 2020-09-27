3d139625af0bebbb058fd6e90c6980ca9342a323
vinay <vinay.negi@thalesesecurity.com>
2020-09-27 05:15:34 +0000

    Updated copyrights in source files

--- Tokenization_Samples/java_samples/src/main/java/com/thales/cts/samples/TokDetok.java
+++ Tokenization_Samples/java_samples/src/main/java/com/thales/cts/samples/TokDetok.java
@@ -2,11 +2,11 @@
 **                                                                      **
 ** Copyright(c) 2020                              Confidential Material **
 **                                                                      **
-** This file is the property of Thales Group.                           **
+** This file is the property of Thales E-security Copyright (c) 2020.   **
 ** The contents are proprietary and confidential.                       **
 ** Unauthorized use, duplication, or dissemination of this document,    **
 ** in whole or in part, is forbidden without the express consent of     **
-** Thales Group.                                                        **
+** Thales E-security Copyright (c) 2020.                                **
 **                                                                      **
 **************************************************************************/
 
