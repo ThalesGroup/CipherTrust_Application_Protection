cbba0aadef4d5b13151594988e07a2874b0ab4ed
markbwarner <markbwarner@gmail.com>
2021-10-18 14:38:59 -0400

    changed constructor for CipherTrustManagerHelper and added new json sample

---
+++ rest/src/main/java/com/thales/cm/rest/cmrestdemo/run-cmrrestjsonlinux.sh
@@ -0,0 +1,8 @@
+export cmdebug=0
+export cmuserid=admin
+export cmpassword=yourpwd
+export cmserver=yourcmserver
+export cmkey=YourAESKey26
+export cmdataformat=alphanumeric
+export cmfile=/buildfiles/messages.json
+java -jar CMRESTJsonDemo.jar
