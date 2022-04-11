dc2c7b74139f25876f21b3ac5019bfe962536db0
Jitendra Mourya <5364025+jiten87@users.noreply.github.com>
2022-04-12 02:14:49 +0530

    Cadp 5136 (#33)
    * Update the repo path and re-branding activity[CADP-5136]
    
    * Deleted pkcs11/VAE/csharp location and update error massages.
    
    * remove introp dll reference
    
    * Added changes to run the sample
    
    Co-authored-by: jitendra mourya <jitendra.mourya@thalesgroup.con>

---
+++ pkcs11/csharp/DigestSample.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Net.Pkcs11Interop.HighLevelAPI.MechanismParams;
+using Net.Pkcs11Interop.Common;
+using Net.Pkcs11Interop.HighLevelAPI;
+
+namespace Vormetric.Pkcs11Sample
+{
+    public class DigestSample: ISample
+    {
+        public bool Run(object[] inputParams)
+        {
+            using (IPkcs11Library pkcs11Library = Settings.Factories.Pkcs11LibraryFactory.LoadPkcs11Library(Settings.Factories, Settings.Pkcs11LibraryPath, Settings.AppType))
+            {
+                // Find first slot with token present
+                ISlot slot = Helpers.GetUsableSlot(pkcs11Library);
+
+                // Open RW session
+                using (ISession session = slot.OpenSession(SessionType.ReadWrite))
+                {
+                    string pin = Convert.ToString(inputParams[0]);
+                    IMechanism mechanism = null;
+                    string opName = "sha256";
+
+                    if (inputParams.Length >= 2)
+                    {
+                        opName = Convert.ToString(inputParams[1]);
+                    }
+                    if (opName.Equals("sha512"))
+                    {
+                        mechanism = session.Factories.MechanismFactory.Create(CKM.CKM_SHA512);
+                    }
+                    else if (opName.Equals("sha384"))
+                    {
+                        mechanism = session.Factories.MechanismFactory.Create(CKM.CKM_SHA384);
+                    }
+                    else if (opName.Equals("sha224"))
+                    {
+                        mechanism = session.Factories.MechanismFactory.Create(CKM.CKM_SHA224);
+                    }
+                    else if (opName.Equals("sha1"))
+                    {
+                        mechanism = session.Factories.MechanismFactory.Create(CKM.CKM_SHA_1);
+                    }
+                    else if (opName.Equals("md5"))
+                    {
+                        mechanism = session.Factories.MechanismFactory.Create(CKM.CKM_MD5);
+                    }
+                    else if (opName.Equals("sha256"))
+                    {
+                        mechanism = session.Factories.MechanismFactory.Create(CKM.CKM_SHA256);
+                        opName = "sha256";
+                    } else {
+                        Console.WriteLine("Only sha1/224/256/384/512 and md5 allowed. This input is case sensitive. hmac not supported in C#.");
+                        return false;
+                    }
+
+                    Console.WriteLine("Mechanism is " + opName);
+
+                    string sourceText = "This is the default Source Text.";
+                    byte[] sourceData = ConvertUtils.Utf8StringToBytes(sourceText);
+
+                    // Login as normal user
+                    session.Login(CKU.CKU_USER, pin);
+
+                    byte[] digestBytes = session.Digest(mechanism, sourceData);
+                    Console.WriteLine("Digest Text: " + ConvertUtils.BytesToHexString(digestBytes));
+
+                    session.Logout();
+                }
+            }
+            return true;
+        }
+
+        public DigestSample()
+        {
+        }
+    }
+}
