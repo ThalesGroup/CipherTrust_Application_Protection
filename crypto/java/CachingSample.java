909fd7b6f76588a616fcb6091b414ae99e99dd44
Thales-AmitRai <139339197+Thales-AmitRai@users.noreply.github.com>
2023-09-15 11:15:56 +0530

    Update CachingSample.java
    Signed-off-by: Thales-AmitRai <139339197+Thales-AmitRai@users.noreply.github.com>

--- crypto/java/CachingSample.java
+++ crypto/java/CachingSample.java
@@ -27,7 +27,7 @@ import com.ingrian.security.nae.NAESessionInterface;
 /**
  * This sample shows how to encrypt and decrypt data using CADP for JAVA in local mode.
  * Note: 1.) Persistent and symmetric cache should be enabled in the IngrainNAE.properties file.
- * 	 2.) This Sample can be used for version and non-version key passed in the argument.
+ * 	 2.) This sample can be used for version and non-version keys passed in the argument.
  */
 
 public class CachingSample 
