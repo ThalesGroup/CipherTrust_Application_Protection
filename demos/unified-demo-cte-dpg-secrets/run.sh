718d0d4158e5fd72a46b33708735217e30e24225
anugram <anurag.jain@thalesgroup.com>
2024-05-06 12:42:33 -0400

    Update run.sh

--- demos/unified-demo-cte-dpg-secrets/run.sh
+++ demos/unified-demo-cte-dpg-secrets/run.sh
@@ -48,7 +48,8 @@ then
   helm repo add akeyless https://akeylesslabs.github.io/helm-charts
   helm repo update
 
-  kubectl apply -f /tmp/namespace.yaml
+  #kubectl apply -f /tmp/namespace.yaml
+  kubectl create ns kubecon
   kubectl label namespace kubecon name=kubecon
 
   CM_IP_WITHOUT_PROTOCOL=$(echo ${CM_IP} | awk -F/ '{print $3}')
@@ -77,7 +78,7 @@ fi
 if [ "$ANSIBLE" = "true" ];
 then
   docker run --detach --privileged --name ansible --volume=/sys/fs/cgroup:/sys/fs/cgroup:rw --volume=/home/aj/.kube:/root/.kube --volume=/tmp:/tmp:rw --cgroupns=host ciphertrust/automation:star-demo-ansible
-  docker exec --tty ansible env TERM=xterm ansible-playbook /root/run_demo.yml -e "CM_IP=$CM_IP" -e "CM_USERNAME=$CM_USERNAME" -e "CM_PASSWORD=$CM_PASSWORD" -e "LOCAL_CA_ID=$CA_ID" -e "ADD_DPG_FLAG=false" -e "SERVER_IP=$KUBE_PUBLIC_IP" -e "SERVER_PORT=9000" -e "NFS_IP=$NFS_SERVER_IP" -e "AKL_ACCESS_ID=$access_id" -e "AKL_ACCESS_KEY=$access_key" -vvv
+  docker exec --tty ansible env TERM=xterm ansible-playbook /root/run_demo.yml -e "CM_IP=$CM_IP" -e "CM_USERNAME=$CM_USERNAME" -e "CM_PASSWORD=$CM_PASSWORD" -e "LOCAL_CA_ID=$CA_ID" -e "ADD_DPG_FLAG=false" -e "SERVER_IP=$KUBE_PUBLIC_IP" -e "SERVER_PORT=9000" -e "NFS_IP=$NFS_SERVER_IP" -e "AKL_ACCESS_ID=$access_id" -e "AKL_ACCESS_KEY=$access_key" -v
 fi
 
 # Install CTE for kubernetes
